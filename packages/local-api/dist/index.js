15bbe4c0a20840b8dac3af8730ed8ef58f712303
Shayne Meyer <shayne.meyer@gmail.com>
2023-01-22 07:36:12 -0800

    updated to support local dev builds vs production builds

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,10 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
-const serve = (port, filename, dir) => {
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const path_1 = __importDefault(require("path"));
+const serve = (port, filename, dir, useProxy) => {
     const app = (0, express_1.default)();
-    app.listen(port, () => {
-        console.log('listening on port', port);
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent',
+        }));
+    }
+    else {
+        const packagePath = require.resolve('local-client/build/index.html');
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
     });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,9 +1,29 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  app.listen(port, () => {
-    console.log('listening on port', port);
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express.static(path.dirname(packagePath)));
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
   });
 };
